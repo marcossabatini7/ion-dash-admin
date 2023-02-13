ccf6d9c3f368ba5ff1f7b7d92ed3863b59a9c22d
Marcos Sabatini <marcos.sabatini@ionsistemas.com.br>
2023-02-13 10:17:25 -0300

    páginas base

---
+++ src/pages/settings.tsx
@@ -0,0 +1,9 @@
+import Layout from '@/components/template/Layout'
+
+export default function Settings() {
+  return (
+    <>
+      <Layout title="Configurações" subtitle="Configure o sistema"></Layout>
+    </>
+  )
+}
