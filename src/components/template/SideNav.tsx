1203304d284493c45c531f071626d0aed6056838
Marcos Sabatini <marcos.sabatini@ionsistemas.com.br>
2023-02-13 10:35:16 -0300

    logo

--- src/components/template/SideNav.tsx
+++ src/components/template/SideNav.tsx
@@ -1,9 +1,13 @@
 import { HomeIcon, NotificationIcon, SettingsIcon } from '../icons'
+import Logo from './Logo'
 import SideNavItem from './SideNavItem'
 
 export default function SideNav() {
   return (
     <aside>
+      <div className="flex flex-col items-center justify-center bg-gradient-to-r from-indigo-500 to-purple-800 h-20 ">
+        <Logo />
+      </div>
       <ul>
         <SideNavItem url="/" text="Home" icon={HomeIcon} key={1} />
         <SideNavItem
